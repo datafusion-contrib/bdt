6b80be26cb2ee491a7ae097c727692865690586f
Andy Grove <andygrove73@gmail.com>
2023-01-04 18:16:37 -0700

    epsilon

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,8 @@ enum Command {
         input1: PathBuf,
         #[structopt(parse(from_os_str))]
         input2: PathBuf,
+        #[structopt(short, long)]
+        epsilon: Option<f64>,
     },
 }
 
@@ -156,8 +158,12 @@ async fn main() -> Result<()> {
         Command::ViewParquetMeta { input } => {
             view_parquet_meta(input)?;
         }
-        Command::Compare { input1, input2 } => {
-            compare_files(input1, input2).await?;
+        Command::Compare {
+            input1,
+            input2,
+            epsilon,
+        } => {
+            compare_files(input1, input2, epsilon).await?;
         }
     }
     Ok(())
@@ -360,7 +366,7 @@ fn file_format(filename: &str) -> Result<FileFormat> {
     }
 }
 
-async fn compare_files(path1: PathBuf, path2: PathBuf) -> Result<bool> {
+async fn compare_files(path1: PathBuf, path2: PathBuf, epsilon: Option<f64>) -> Result<bool> {
     let ctx = SessionContext::new();
     // TODO assumes csv for now
     let df = ctx
@@ -383,11 +389,28 @@ async fn compare_files(path1: PathBuf, path2: PathBuf) -> Result<bool> {
             if a.len() == b.len() {
                 for (j, (v1, v2)) in a.iter().zip(b.iter()).enumerate() {
                     if v1 != v2 {
-                        println!(
-                            "data does not match at row {} column {}: {:?} != {:?}",
-                            i, j, v1, v2
-                        );
-                        return Ok(false);
+                        let ok = if let Some(epsilon) = epsilon {
+                            match (v1, v2) {
+                                (
+                                    ScalarValue::Float32(Some(ll)),
+                                    ScalarValue::Float32(Some(rr)),
+                                ) => ((ll - rr) as f64) < epsilon,
+                                (
+                                    ScalarValue::Float64(Some(ll)),
+                                    ScalarValue::Float64(Some(rr)),
+                                ) => ll - rr < epsilon,
+                                _ => false,
+                            }
+                        } else {
+                            false
+                        };
+                        if !ok {
+                            println!(
+                                "data does not match at row {} column {}: {:?} != {:?}",
+                                i, j, v1, v2
+                            );
+                            return Ok(false);
+                        }
                     }
                 }
             } else {
@@ -424,84 +447,97 @@ impl RowIter {
 }
 
 impl Iterator for RowIter {
-    type Item = Vec<Option<ScalarValue>>;
+    type Item = Vec<ScalarValue>;
 
     fn next(&mut self) -> Option<Self::Item> {
         while self.current_batch < self.batches.len() {
             let b = &self.batches[self.current_batch];
             if self.current_batch_offset < b.num_rows() {
-                let mut row: Vec<Option<ScalarValue>> = Vec::with_capacity(b.num_columns());
+                let mut row: Vec<ScalarValue> = Vec::with_capacity(b.num_columns());
                 let row_index = self.current_batch_offset;
                 self.current_batch_offset += 1;
                 for col_index in 0..b.num_columns() {
                     let array = b.column(col_index);
                     if array.is_null(row_index) {
-                        row.push(None)
+                        row.push(ScalarValue::Null)
                     } else {
                         match array.data_type() {
                             DataType::Utf8 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::StringArray>().unwrap();
-                                row.push(Some(ScalarValue::Utf8(Some(
+                                row.push(ScalarValue::Utf8(Some(
                                     array.value(row_index).to_string(),
-                                ))));
+                                )));
                             }
                             // TODO introduce macros to make this concise
                             DataType::Int8 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::Int8Array>().unwrap();
-                                row.push(Some(ScalarValue::Int8(Some(array.value(row_index)))));
+                                row.push(ScalarValue::Int8(Some(array.value(row_index))));
                             }
                             DataType::Int16 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::Int16Array>().unwrap();
-                                row.push(Some(ScalarValue::Int16(Some(array.value(row_index)))));
+                                row.push(ScalarValue::Int16(Some(array.value(row_index))));
                             }
                             DataType::Int32 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::Int32Array>().unwrap();
-                                row.push(Some(ScalarValue::Int32(Some(array.value(row_index)))));
+                                row.push(ScalarValue::Int32(Some(array.value(row_index))));
                             }
                             DataType::Int64 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::Int64Array>().unwrap();
-                                row.push(Some(ScalarValue::Int64(Some(array.value(row_index)))));
+                                row.push(ScalarValue::Int64(Some(array.value(row_index))));
                             }
                             DataType::UInt8 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::UInt8Array>().unwrap();
-                                row.push(Some(ScalarValue::UInt8(Some(array.value(row_index)))));
+                                row.push(ScalarValue::UInt8(Some(array.value(row_index))));
                             }
                             DataType::UInt16 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::UInt16Array>().unwrap();
-                                row.push(Some(ScalarValue::UInt16(Some(array.value(row_index)))));
+                                row.push(ScalarValue::UInt16(Some(array.value(row_index))));
                             }
                             DataType::UInt32 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::UInt32Array>().unwrap();
-                                row.push(Some(ScalarValue::UInt32(Some(array.value(row_index)))));
+                                row.push(ScalarValue::UInt32(Some(array.value(row_index))));
                             }
                             DataType::UInt64 => {
                                 let array =
                                     array.as_any().downcast_ref::<array::UInt64Array>().unwrap();
-                                row.push(Some(ScalarValue::UInt64(Some(array.value(row_index)))));
+                                row.push(ScalarValue::UInt64(Some(array.value(row_index))));
                             }
                             DataType::Float32 => {
                                 let array = array
                                     .as_any()
                                     .downcast_ref::<array::Float32Array>()
                                     .unwrap();
-                                row.push(Some(ScalarValue::Float32(Some(array.value(row_index)))));
+                                row.push(ScalarValue::Float32(Some(array.value(row_index))));
                             }
                             DataType::Float64 => {
                                 let array = array
                                     .as_any()
                                     .downcast_ref::<array::Float64Array>()
                                     .unwrap();
-                                row.push(Some(ScalarValue::Float64(Some(array.value(row_index)))));
+                                row.push(ScalarValue::Float64(Some(array.value(row_index))));
                             }
-                            _ => todo!("unsupported data type"),
+                            DataType::Date32 => {
+                                let array =
+                                    array.as_any().downcast_ref::<array::Date32Array>().unwrap();
+                                row.push(ScalarValue::Date32(Some(array.value(row_index))));
+                            }
+                            DataType::Date64 => {
+                                let array =
+                                    array.as_any().downcast_ref::<array::Date64Array>().unwrap();
+                                row.push(ScalarValue::Date64(Some(array.value(row_index))));
+                            }
+                            other => {
+                                println!("unsupported type: {}", other);
+                                todo!("unsupported data type")
+                            }
                         }
                     }
                 }
